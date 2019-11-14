b86b84f17b73a1cf7264cac2bf65e1c5051a571b
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-14 21:50:29 +0900

    회원가입 화면 초안

---
+++ app/src/main/java/com/tjoeun/a20191114_01_okhttp/SignUpActivity.kt
@@ -0,0 +1,18 @@
+package com.tjoeun.a20191114_01_okhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}
