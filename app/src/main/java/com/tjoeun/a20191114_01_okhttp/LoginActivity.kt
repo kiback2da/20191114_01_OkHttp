ed970c3f3c04f92d65edd8c2c6e64bdc247ff6ce
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-14 21:38:16 +0900

    로그인 응답에 따른 다른 토스트 띄우기

--- app/src/main/java/com/tjoeun/a20191114_01_okhttp/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a20191114_01_okhttp/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.tjoeun.a20191114_01_okhttp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.tjoeun.a20191114_01_okhttp.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
 
@@ -17,7 +20,22 @@ class LoginActivity : BaseActivity() {
         loginBtnLogin.setOnClickListener {
             var inputId = loginEdtID.text.toString()
             var inputPW =  loginEdtPassword.text.toString()
-            ServerUtil.postRequestLogin(mContext, inputId,inputPW,null)
+            ServerUtil.postRequestLogin(mContext, inputId,inputPW,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+                    runOnUiThread {
+                        Log.d("로그","${json.toString()}")
+
+                        val code = json.getInt("code")
+                        val message = json.getString("message")
+
+                        if(code == 400){
+                            Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show()
+                        }else if(code == 200){
+                            Toast.makeText(mContext,"로그인 성공",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
         }
     }
 
